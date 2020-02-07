8c889b0edda63fe181c15c587da0234e754d993f
andressaggudelo <cagudeloa@uninorte.edu.co>
2020-02-07 14:45:27 -0500

    Sharing menu and Intents added

--- app/src/main/java/com/andressaggudelo/navigation/ConfirmationFragment.kt
+++ app/src/main/java/com/andressaggudelo/navigation/ConfirmationFragment.kt
@@ -1,12 +1,12 @@
 package com.andressaggudelo.navigation
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import com.andressaggudelo.navigation.databinding.FragmentConfirmationBinding
 
@@ -22,12 +22,41 @@ class ConfirmationFragment : Fragment() {
 
         val binding: FragmentConfirmationBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_confirmation, container, false)
 
-        var args = arguments?.let { ConfirmationFragmentArgs.fromBundle(it) }
 
-        Toast.makeText(context, "Sent money: ${args?.amount}", Toast.LENGTH_LONG).show()
+
+        //Toast.makeText(context, "Sent money: \$${args?.amount}", Toast.LENGTH_LONG).show()
+
+        setHasOptionsMenu(true)
+
 
         return inflater.inflate(R.layout.fragment_confirmation, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share_menu, menu)
+        if(null == getShareIntent().resolveActivity(activity!!.packageManager)){
+            menu.findItem(R.id.share).setVisible(false)
+        }
+    }
+
+    private fun getShareIntent(): Intent{
+        var args = arguments?.let { ConfirmationFragmentArgs.fromBundle(it) }   // Brought my data from the other Fragment
+        return ShareCompat.IntentBuilder.from(activity)
+            .setText(getString(R.id.cancel_amount.toInt(), args?.amount))
+            .setType("text/plain")
+            .intent
+    }
+
+    private fun shareSuccess(){
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
